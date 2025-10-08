a1dc41f6c6a4513dee9731a2d61440fd70569117
bigitla <bigitla@protonmail.com>
2025-10-08 10:12:41 +0300

    Update mailbox.js

--- functions/mailbox.js
+++ functions/mailbox.js
@@ -1,1 +1,24 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const kv = env.MAILBOX;
+
+  if (request.method === 'POST') {
+    const { message } = await request.json();
+    const id = Date.now().toString();
+    await kv.put(id, message);
+    return new Response("Saved", { status: 200 });
+  }
+
+  if (request.method === 'GET') {
+    const list = await kv.list();
+    const messages = await Promise.all(
+      list.keys.map(async key => await kv.get(key.name))
+    );
+    return new Response(JSON.stringify(messages), {
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+
+  return new Response("Method not allowed", { status: 405 });
+}
 
